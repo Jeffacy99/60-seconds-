6abeaf8c512eaeff9e2fdf40c299d9158fe2f222
Jeffacy99 <mwaniajapheth1999@gmail.com>
2020-01-14 10:30:40 +0300

    manage.py add server

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@ from app import create_app, db
 from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
+..........
+app = create_app('production')
+..........
+
 from app.models import User
 # Creating app instance
 app = create_app('production')
